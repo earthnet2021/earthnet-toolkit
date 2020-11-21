e83f5116d8d5c56891a5ea12c3a985b06cf3a8d7
Vitus Benson <vbenson@bgc-jena.mpg.de>
2020-11-21 22:10:02 +0100

    Added Setup.py and first init

---
+++ earthnet/__init__.py
@@ -0,0 +1,9 @@
+"""EarthNet2021 Toolkit
+A library for downloading, evaluating and plotting Earth surface forecasts.
+"""
+__version__ = "0.1.2"
+__author__ = 'Vitus Benson, Christian Requena-Mesa'
+__credits__ = 'Max-Planck-Institute for Biogeochemistry'
+
+
+
