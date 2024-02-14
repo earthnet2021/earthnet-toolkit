78984aa8d3b013e435b667e1d741d464da940939
Vitus Benson <33334860+vitusbenson@users.noreply.github.com>
2024-02-14 21:31:18 +0100

    Update __init__.py

--- earthnet/__init__.py
+++ earthnet/__init__.py
@@ -1,7 +1,7 @@
 """EarthNet2021 Toolkit
 A library for downloading, evaluating and plotting Earth surface forecasts.
 """
-__version__ = "0.3.9"
+__version__ = "0.3.10"
 __author__ = 'Vitus Benson, Christian Requena-Mesa'
 __credits__ = 'Max-Planck-Institute for Biogeochemistry'
 
